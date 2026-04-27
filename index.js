a392eccf3ef4740c03bd97097d8ec519970bb17f
lucianalopezag-lang <luciana.lopezag@gmail.com>
2026-04-26 20:33:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const auth = (req, res, next) => {
   if (!token) return res.status(401).send("No token");
 
   try {
-    const decoded = jwt.verify(token, "secret_key");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch {
