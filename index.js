804db16c3628d2fd1681f2aab71be0936f889bd1
lucianalopezag-lang <luciana.lopezag@gmail.com>
2026-04-28 16:57:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const jwt = require("jsonwebtoken");
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
